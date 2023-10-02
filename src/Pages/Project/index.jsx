270d47d31f690d1e16a0312639338c9e2e94944f
Muhamad-Farhannudin <110012265+Muhamad-Farhannudin@users.noreply.github.com>
2023-10-02 17:48:23 +0700

    repair responsive mobile

--- src/Pages/Project/index.jsx
+++ src/Pages/Project/index.jsx
@@ -9,8 +9,8 @@ export default function index() {
   const projects = projectData.projects;
 
   return (
-    <Fade bottom duration={1500}>
-      <div id="projects" className="w-[75%] lg:w-[50%] pt-24 absolute top-[430%] md:top-[250%] lg:right-24" aria-label="Selected projects">
+    <Fade right>
+      <div id="projects" className="w-[75%] lg:w-[50%] pt-24 absolute top-[590%] md:top-[250%] lg:right-24" aria-label="Selected projects">
         <div className="sticky top-0 z-20 -mx-6 mb-4 bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
           <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Projects</h2>
         </div>
